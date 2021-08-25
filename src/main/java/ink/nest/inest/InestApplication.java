5d017d335d251efee2f777294cc4db790b88ed72
rezazadeh93 <amin.rezazadeh93@gmail.com>
2021-08-25 17:28:00 +0430

    init project

---
+++ src/main/java/ink/nest/inest/InestApplication.java
@@ -0,0 +1,13 @@
+package ink.nest.inest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ink/nest/inest/InestApplicationTests.java
@@ -0,0 +1,13 @@
+package ink.nest.inest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

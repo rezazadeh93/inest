12ce6d880555c8892fdd97dc2140b343ef5e8bd3
rezazadeh93 <amin.rezazadeh93@gmail.com>
2021-09-13 16:27:17 +0430

    #7 Implemented All Endpoints in LinkController

--- src/main/java/ink/nest/inest/controller/v1/LinkController.java
+++ src/main/java/ink/nest/inest/controller/v1/LinkController.java
@@ -1,38 +1,38 @@
 package ink.nest.inest.controller.v1;
 
-import ink.nest.inest.api.v1.model.AccountDTO;
 import ink.nest.inest.api.v1.model.LinkDTO;
 import ink.nest.inest.constant.InestApiConstant;
+import ink.nest.inest.domain.Account;
 import ink.nest.inest.security.JwtTokenUtil;
+import ink.nest.inest.service.AccountCrudService;
 import ink.nest.inest.service.LinkCrudService;
-import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.server.ResponseStatusException;
 
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
 import java.util.Set;
 
 @RestController
 @RequestMapping(InestApiConstant.API_V1_PATH)
 public class LinkController {
     private final LinkCrudService linkCrudService;
+    private final AccountCrudService accountCrudService;
     private final JwtTokenUtil jwtTokenUtil;
 
-    public LinkController(LinkCrudService linkCrudService, JwtTokenUtil jwtTokenUtil) {
+    public LinkController(LinkCrudService linkCrudService,
+                          AccountCrudService accountCrudService,
+                          JwtTokenUtil jwtTokenUtil) {
         this.linkCrudService = linkCrudService;
+        this.accountCrudService = accountCrudService;
         this.jwtTokenUtil = jwtTokenUtil;
     }
 
     @GetMapping("links")
-    public Set<LinkDTO> getAll(HttpServletRequest request) {
+    public Set<LinkDTO> getAll(WebRequest request) {
         //get token from header
-        String token = request.getHeader(InestApiConstant.HEADER_AUTHORIZATION)
+        String token = Objects.requireNonNull(request.getHeader(InestApiConstant.HEADER_AUTHORIZATION))
                 .replace(InestApiConstant.HEADER_BEARER, "");
 
         //get username from token
@@ -50,8 +50,24 @@ public class LinkController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("link")
-    public LinkDTO create() {
-        //@todo need implement
-        return null;
+    public LinkDTO createLink(@RequestParam("name") String name, WebRequest request) {
+        //get token from header
+        String token = Objects.requireNonNull(request.getHeader(InestApiConstant.HEADER_AUTHORIZATION))
+                .split(" ")[1];
+
+        Account activeAccount = accountCrudService.findByEmail(jwtTokenUtil.getUsername(token))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Not Found Account"));
+
+        return linkCrudService.saveLinkDTO(name, activeAccount)
+                .orElseThrow(
+                        () -> new ResponseStatusException(
+                                HttpStatus.INTERNAL_SERVER_ERROR,
+                                "Something went wrong during saving account"
+                        )
+                );
+    }
+
+    public void deleteLink(@RequestParam("id") Long id) {
+        linkCrudService.softDeleteByIdAccount(id);
     }
 }
--- src/main/java/ink/nest/inest/domain/Link.java
+++ src/main/java/ink/nest/inest/domain/Link.java
@@ -1,8 +1,11 @@
 package ink.nest.inest.domain;
 
 import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -12,6 +15,9 @@ import java.util.Set;
 @Entity
 @ToString(exclude = {"account"})
 @EqualsAndHashCode(exclude = {"account"})
+
+@SQLDelete(sql = "UPDATE link SET deleted = true WHERE id=?")
+@Where(clause = "deleted=false")
 public class Link {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -27,4 +33,9 @@ public class Link {
 
     @OneToOne(cascade = CascadeType.DETACH)
     private Templates template;
+
+    private boolean deleted = Boolean.FALSE;
+
+    private LocalDate createAt;
+    private LocalDate updatedAt;
 }
